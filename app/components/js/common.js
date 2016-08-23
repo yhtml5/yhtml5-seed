959d971000fed4c52bd21a46195c8aca542731ee
张大漾丨前端开发 <1417376255@qq.com>
2016-08-23 11:06:55 +0800

    11

--- app/components/js/common.js
+++ app/components/js/common.js
@@ -6,3 +6,26 @@ if (isBrowserMobile) {
 } else {
     console.log(a);
 }
+
+
+function userBrowser(){
+    var browserName=navigator.userAgent.toLowerCase();
+    if(/msie/i.test(browserName) && !/opera/.test(browserName)){
+        alert("IE");
+        return ;
+    }else if(/firefox/i.test(browserName)){
+        alert("Firefox");
+        return ;
+    }else if(/chrome/i.test(browserName) && /webkit/i.test(browserName) && /mozilla/i.test(browserName)){
+        alert("Chrome");
+        return ;
+    }else if(/opera/i.test(browserName)){
+        alert("Opera");
+        return ;
+    }else if(/webkit/i.test(browserName) &&!(/chrome/i.test(browserName) && /webkit/i.test(browserName) && /mozilla/i.test(browserName))){
+        alert("Safari");
+        return ;
+    }else{
+        alert("unKnow");
+    }
+}
