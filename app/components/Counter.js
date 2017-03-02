10cddd8506d1cd1bd33c079eeb4b857e02ab4243
张大漾丨前端开发 <1417376255@qq.com>
2017-03-02 16:17:13 +0800

    auto update

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -2,29 +2,29 @@ import React, {Component} from 'react';
 import num from './test.js'
 
 export default class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {counter: 0};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {counter: 0};
+  }
 
-    componentDidMount() {
-        this.interval = setInterval(this.tick.bind(this), 1000);
-    }
+  componentDidMount() {
+    this.interval = setInterval(this.tick.bind(this), 1000);
+  }
 
-    tick() {
-        this.setState({
-            counter: this.state.counter + num
-        });
-    }
+  tick() {
+    this.setState({
+      counter: this.state.counter + num
+    });
+  }
 
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
-    render() {
-        return (
-            <h2>Counter: {this.state.counter}</h2>
-        );
-    }
+  render() {
+    return (
+      <h2>Counter: {this.state.counter}</h2>
+    );
+  }
 }
 
