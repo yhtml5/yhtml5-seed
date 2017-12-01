f88e7aec2d4ece589d1b98c2fb5634c13f2515f4
张大漾 <1417376255@qq.com>
2017-12-01 16:44:59 +0800

    update: yhtml5-scripts

--- apps/react-dashboard/.config.js
+++ apps/react-dashboard/.config.js
@@ -3,24 +3,26 @@
  * Description: The configuration file for the yhtml5-scripts should not be packaged into the app
  *
  */
+const packageJson = require('./package.json')
+const { getVersion } = require('yhtml5-dev-utils')
+const outputPath = `dist/${getVersion(packageJson.version)}`
 
 const config = {
   devHost: '0.0.0.0',
   devPort: 9991,
+  isAnalyze: true,
   analyzerPort: 9992,
-  isAnalyze : true,
+  outputPath: outputPath,
   // host: './',
 
   entry: 'src/index.js',
   distributePort: 9993,
-  output: '/',
   type: '',
-  templateHtml: 'src/pages/template.js',
   test: {
-    // moduleNameMapper: webpackConfigAlias,
     testMatch: ['app/**/__tests__/**/*.js?(x)', 'app/**/?(*.)(spec|test).js?(x)'],
     transformIgnorePatterns: ["node_modules/(?!(yhtml5-test|react-redux|react-native-button)/)"],
-    collectCoverageFrom: ['app/**/*.{js,jsx}']
+    collectCoverageFrom: ['app/**/*.{js,jsx}'],
+    // moduleNameMapper: webpackConfigAlias,
   }
 }
 
--- apps/react-dashboard/src/index.js
+++ apps/react-dashboard/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Router from './Router.jsx'
-import './global.pcss'
+import './global.css'
 
 const render = (Components) => {
   ReactDOM.render(
